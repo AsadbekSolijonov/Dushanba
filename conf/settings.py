1e90c938791106b9107568db8ad7d32f1cc63399
Asadbek <iamsolijonovasadbek@gmail.com>
2023-02-20 23:56:17 +0500

    template

--- conf/settings.py
+++ conf/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates_2'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
